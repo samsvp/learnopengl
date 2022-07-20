c03ed1e38aa74972a105200891a6e1face4c11f3
Samuel Simplicio <samuelsimplicio5@gmail.com>
2022-07-19 22:01:19 -0300

    Hello Triangle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <glad/glad.h>
+#include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
 
@@ -44,6 +44,113 @@ int main()
 
     glViewport(0, 0, 800, 600);
 
+    // triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // tell opengl how to interpret the vertex data
+    /*
+
+    The first parameter specifies which vertex attribute we want to configure. Remember that we 
+    specified the location of the position vertex attribute in the vertex shader with layout (location = 0).
+    This sets the location of the vertex attribute to 0 and since we want to pass data to 
+    this vertex attribute, we pass in 0.
+
+    The next argument specifies the size of the vertex attribute. The vertex attribute is a vec3 so 
+    it is composed of 3 values.
+
+    The third argument specifies the type of the data which is GL_FLOAT.
+
+    The next argument specifies if we want the data to be normalized. If we're inputting integer data 
+    types (int, byte) and we've set this to GL_TRUE, the integer data is normalized to 0 
+    (or -1 for signed data) and 1 when converted to float.
+
+    The fifth argument is known as the stride and tells us the space between consecutive vertex attributes.
+    Since the next set of position data is located exactly 3 times the size of a float away we 
+    specify that value as the stride. Note that since we know that the array is tightly packed 
+    (there is no space between the next vertex attribute value) we could've also specified the stride as 0 
+    to let OpenGL determine the stride (this only works when values are tightly packed). 
+    Whenever we have more vertex attributes we have to carefully define the spacing between each 
+    vertex attribute but we'll get to see more examples of that later on.
+
+    The last parameter is of type void* and thus requires that weird cast. This is the offset of 
+    where the position data begins in the buffer. Since the position data is at the start of the 
+    data array this value is just 0. We will explore this parameter in more detail later on
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // compile shader
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check if shader was compiled successfully
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // link shaders to shader program
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // check for errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) 
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // use the program and delete the shaders now that they have been attached to the program
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // bind the VAO
+    glBindVertexArray(VAO);
+
     while (!glfwWindowShouldClose(window))
     {
         // input
@@ -52,6 +159,7 @@ int main()
         // render
         glClearColor(0.5, 0.0, 0.5, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // call events and swap buffers
         glfwSwapBuffers(window);
